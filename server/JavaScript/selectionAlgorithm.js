770a55917c234ab87fb64a355c456c30684af7ba
AJTootell <up770605@myport.ac.uk>
2019-03-19 02:09:35 +0000

    filters media back-end complete

--- server/JavaScript/selectionAlgorithm.js
+++ server/JavaScript/selectionAlgorithm.js
@@ -11,6 +11,26 @@ function mediaQueryCB(err,data){
 
   media = data;
 
+  var query = 'select * from rdc01hn4hfiuo1rv.usermed where user_id = '+userId+';';
+  util.queryDB(query, usermedQueryCB);
+}
+
+/*
+removed alredy viewed media
+*/
+
+function usermedQueryCB(err, data){
+  var newMedia = media.filter(function(value, index, arr){
+    for (var j=0; j<data.length; j++){
+      if (value.med_id == data[j].med_id){
+        return false;
+      }
+    }
+    return true;
+  });
+
+  media = newMedia;
+
   var query = 'select * from rdc01hn4hfiuo1rv.user where user_id = '+userId+';';
   util.queryDB(query, userQueryCB);
 }
@@ -54,60 +74,96 @@ function userQueryCB(err,data){
   } else {
     dateFilteredMedia = typeFilteredMedia;
   }
-/*
-  //filter by location
-  var region = false,
-  country = false,
-  location = false;
 
-  if (userData.reg_id){
-    region = true;
-    if (userData.cou_id){
-      country = true;
-      if (userData.loc_id){
-        location = true;
-      }
-    }
-  }
+  //filter by location
 
   var locFilteredMedia = [];
 
-  for (var i=0;i<dateFilteredMedia.length;i++){
-    if (dateFilteredMedia[i].med_type == "photo" && userData.user_showPhoto == '1'){
-      locFilteredMedia.push(dateFilteredMedia[i])
-    }
-    if (dateFilteredMedia[i].med_type == "video" && userData.user_showVideo == '1'){
-      locFilteredMedia.push(dateFilteredMedia[i])
-    }
-    if (dateFilteredMedia[i].med_type == "audio" && userData.user_showAudio == '1'){
-      locFilteredMedia.push(dateFilteredMedia[i])
+  if (userData.reg_id){
+    for (var i=0;i<dateFilteredMedia.length;i++){
+      if (dateFilteredMedia[i].reg_id && dateFilteredMedia[i].reg_id == userData.reg_id){
+        if (userData.cou_id){
+          if (dateFilteredMedia[i].cou_id && dateFilteredMedia[i].cou_id == userData.cou_id){
+            if (userData.loc_id){
+              if (dateFilteredMedia[i].loc_id && dateFilteredMedia[i].loc_id == userData.loc_id){
+                //region, country and location match
+                locFilteredMedia.push(dateFilteredMedia[i]);
+              }
+            }
+            else{
+              //region and country match and no location filter given
+              locFilteredMedia.push(dateFilteredMedia[i]);
+            }
+          }
+        }
+        else{
+          //region match and no country or location filter given
+          locFilteredMedia.push(dateFilteredMedia[i]);
+        }
+      }
+      // end of loop
     }
   }
+  else {
+    //no filter based on location of media
+    locFilteredMedia = dateFilteredMedia;
+  }
 
+  media = locFilteredMedia;
+  query = 'select * from rdc01hn4hfiuo1rv.medcate';
 
-*/
-  media = dateFilteredMedia;
+  util.queryDB(query,medcateQueryCB);
 
-  var query = 'select * from rdc01hn4hfiuo1rv.usermed where user_id = '+userId+';';
-  util.queryDB(query, usermedQueryCB);
 }
 
 /*
-removed alredy viewed media
+Add array of categories for each media
 */
 
+function medcateQueryCB(err,data){
 
-function usermedQueryCB(err, data){
-  var newMedia = media.filter(function(value, index, arr){
-    for (var j=0; j<data.length; j++){
-      if (value.med_id == data[j].med_id){
-        return false;
+  for (var i=0;i<data.length;i++){
+    for (var j=0;i<media.length;j++){
+      if(data[i].med_id == media[j].med_id){
+        if (media[j].hasOwnProperty('categories')){
+          media[j].categories.push(data[i].cate_id);
+        }
+        else{
+          media[j].categories = [data[i].cate_id];
+        }
       }
     }
-    return true;
-  });
+  }
 
-  media = newMedia
+  query = 'select * from rdc01hn4hfiuo1rv.usercate where user_id = '+userId;
+
+  util.queryDB(query,usercateQueryCB);
+}
+
+/*
+Add each media every time based on combined weights of it's categories
+*/
+
+function usercateQueryCB(err,data){
+
+  var multipliedMedia = [];
+
+  if (!data){
+    selectRandom();
+    return;
+  }
+
+  for (var i=0; i<data.length;i++){
+    for (var j=0;j<media.length;j++){
+      if (media[j].categories.includes(data[i].cate_id)) {
+        for (var k=0;k<data[i].usercate_weight;k++){
+          multipleMedia.push(media[j]);
+        }
+      }
+    }
+  }
+
+  media = multipleMedia;
 
   selectRandom();
 }
